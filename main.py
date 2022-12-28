96b3a92f490946f71f99376aa27b749e12ff4e17
pedro donoso <pedro.donosoa@sansano.usm.cl>
2022-12-28 18:31:27 -0300

    finish commit

--- main.py
+++ main.py
@@ -22,12 +22,13 @@ MAX_ATTEMPTS = int(os.environ['MAX_ATTEMPTS'])
 WAIT_CONNECTION = int(os.environ['WAIT_CONNECTION'])
 
 try:
-    #RDK = Robolink(args=['/NOSPLASH','/NOSHOW'])
-    RDK = Robolink(args=['/NOUI', '/NOSPLASH','/NOSHOW', '/HIDDEN', '/NO_WINDOWS'])
-    RDK.setWindowState(windowstate=WINDOWSTATE_HIDDEN)
-    RDK.HideRoboDK()
+    # RDK = Robolink(args=['/NOUI', '/NOSPLASH','/NOSHOW', '/HIDDEN', '/NO_WINDOWS']) # args no funcionan
+    RDK = Robolink()
+    RDK.setWindowState(windowstate=WINDOWSTATE_HIDDEN) # esconder ventana de carga ejecutable
+    RDK.HideRoboDK() # esconder vista robodk
+
     RDK.setRunMode(RUNMODE_RUN_ROBOT) # descomentar para probar en robot
-    # RDK.setRunMode(RUNMODE_SIMULATE )
+    # RDK.setRunMode(RUNMODE_SIMULATE ) # descomentar para pruebas en software
 
     # crea link RDK
     link = Link(rdk=RDK, robot_name=ROBOT_NAME, path_station=PATH_STATION, robot_ip=ROBOT_IP, m_a=MAX_ATTEMPTS, w_c=WAIT_CONNECTION)
@@ -37,25 +38,18 @@ try:
 
     # crea instancia Item Robot 
     link.create_robot()
-    
-    print("Robot creado!!!!!!!")
+
     # imprime nombre del Robot y estación activa de trabajo
     link.print_robot()
 
     # genera conexión con robot
     link.connection_robot()
-    print("Robot conectado!!!!!")
 
-    link.print_robot()
     # instancia la clase para realizar movimientos
     moves = Moves(robot=link.robot)
-    link.print_robot()
 
     # realiza movimiento de prueba
     moves.movement_test()
-    link.print_robot()
 except Exception as e:
     print("Terminado except: {}".format(e))
     exit()
-    # quit(1)
-
